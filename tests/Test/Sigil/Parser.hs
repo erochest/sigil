be22dce82209f893c427fd64344ad520caa135aa
Eric Rochester <err8n@virginia.edu>
2012-06-05 08:32:39 -0400

    More tests for Language.Sigil.Parser.

--- tests/Test/Sigil/Parser.hs
+++ tests/Test/Sigil/Parser.hs
@@ -4,25 +4,22 @@ module Test.Sigil.Parser
     ( parserTests
     ) where
 
-import           Control.Applicative
-import qualified Data.Char as C
-import           Data.Monoid
 import qualified Data.Text as T
 import           Language.Sigil
 import           Test.HUnit (Assertion, assertBool)
 import           Test.Framework (Test, testGroup)
 import           Test.Framework.Providers.HUnit (testCase)
-import           Test.Framework.Providers.QuickCheck2 (testProperty)
-import           Test.QuickCheck
 import           Text.Printf
 
 -- Utilities
 
-assertLeft :: String -> T.Text -> Assertion
-assertLeft msg input =
-    assertBool msg' $ either (const True) (const False) actual
-    where msg'   = printf "%s %s => %s" msg (show input) (show actual)
-          actual = parseText input
+{-
+ - assertLeft :: String -> T.Text -> Assertion
+ - assertLeft msg input =
+ -     assertBool msg' $ either (const True) (const False) actual
+ -     where msg'   = printf "%s %s => %s" msg (show input) (show actual)
+ -           actual = parseText input
+ -}
 
 assertParse :: String -> SWord -> T.Text -> Assertion
 assertParse msg expected input =
@@ -65,88 +62,77 @@ assertLiteralInt = do
     where a :: Int -> T.Text -> Assertion
           a = assertParse "assertLiteralInt" . I
 
-{-
- - assertInstructionAlpha :: Assertion
- - assertInstructionAlpha = do
- -     a "aaa" "aaa"
- -     a "bbb" "BBB"
- -     a "ccc" "CcC"
- -     a "abcdefghijklmnopqrstuvwxyz" "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
- -     where
- -         a :: T.Text -> T.Text -> Assertion
- -         a = assertParse "assertInstructionAlpha"
- - 
- - assertInstructionAlphaNum :: Assertion
- - assertInstructionAlphaNum = do
- -     a "a888" "A888"
- -     a "g9h0i1" "G9h0I1"
- -     where
- -         a :: T.Text -> T.Text -> Assertion
- -         a = assertParse "assertInstructionAlphaNum"
- - 
- - assertInstructionSymbol :: Assertion
- - assertInstructionSymbol = do
- -     a "a+9" "A+9"
- -     a "exec.do*times" "EXEC.DO*TIMES"
- -     a "float.%" "FLOAT.%"
- -     a "float.*" "FLOAT.*"
- -     a "float.+" "FLOAT.+"
- -     a "float.-" "FLOAT.-"
- -     a "float./" "FLOAT./"
- -     a "float.<" "FLOAT.<"
- -     a "float.=" "FLOAT.="
- -     a "float.>" "FLOAT.>"
- -     where
- -         a :: T.Text -> T.Text -> Assertion
- -         a = assertParse "assertInstructionSymbol"
- - 
- - assertInstructionsDot :: Assertion
- - assertInstructionsDot = do
- -     a "a.+" "A.+"
- -     a "integer.+" "INTEGER.+"
- -     a "boolean.dup" "BOOLEAN.DUP"
- -     where
- -         a :: T.Text -> T.Text -> Assertion
- -         a = assertParse "assertInstructionsDot"
- - 
- - assertProgramEmpty :: Assertion
- - assertProgramEmpty = do
- -     a "()"
- -     a "( )"
- -     a "(\n)"
- -     where a = assertParse "assertProgramEmpty"
- - 
- - assertProgramSingle :: Assertion
- - assertProgramSingle = do
- -     a [B True]        "(true)"
- -     a [F 3.14]        "(3.14)"
- -     a [I 42]          "(42)"
- -     a [N "integer.+"] "(INTEGER.+)"
- -     where a = assertParse "assertProgramSingle"
- - 
- - assertProgramExtraWS :: Assertion
- - assertProgramExtraWS = do
- -     a [B True]        "( true )"
- -     a [F 3.14]        "( 3.14 )"
- -     a [I 42]          "( 42 )"
- -     a [N "integer.+"] "( INTEGER.+ )"
- -     where a = assertParse "assertProgramExtraWS"
- - 
- - assertProgramEmbedded :: Assertion
- - assertProgramEmbedded = do
- -     a [C [B True]] "( (TRUE) )"
- -     a [C [I 42  ]] "( ( 42 ) )"
- -     a [C [C [F 3.141592]]] "(((3.141592)) )"
- -     where a = assertParse "assertProgramEmbedded"
- - 
- - assertProgramMultiple :: Assertion
- - assertProgramMultiple = do
- -     a [I 42, I 13, F 3.141592] "( 42 13 3.141592 )"
- -     a [I 42, C [I 13, F 3.141592], N "integer.+"] "(42 (13 3.141592) INTEGER.+)"
- -     a [C [N "a", N "b"], C [N "d", N "e"], C [N "f", N "g"]]
- -            "( (a b) ( d e ) (f g) )"
- -     where a = assertParse "assertProgramMultiple"
- -}
+assertInstructionAlpha :: Assertion
+assertInstructionAlpha = do
+    a "aaa" "aaa"
+    a "bbb" "BBB"
+    a "ccc" "CcC"
+    a "abcdefghijklmnopqrstuvwxyz" "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    where
+        a :: T.Text -> T.Text -> Assertion
+        a = assertParse "assertInstructionAlpha" . S
+
+assertInstructionAlphaNum :: Assertion
+assertInstructionAlphaNum = do
+    a "a888" "A888"
+    a "g9h0i1" "G9h0I1"
+    where
+        a :: T.Text -> T.Text -> Assertion
+        a = assertParse "assertInstructionAlphaNum" . S
+
+assertInstructionSymbol :: Assertion
+assertInstructionSymbol = do
+    a "+" "+"
+    a "*" "*"
+    a "%" "%"
+    a "-" "-"
+    a "/" "/"
+    a "<" "<"
+    a "=" "="
+    a ">" ">"
+    where
+        a :: T.Text -> T.Text -> Assertion
+        a = assertParse "assertInstructionSymbol" . S
+
+assertProgramEmpty :: Assertion
+assertProgramEmpty = do
+    a "[]"
+    a "[ ]"
+    a "[\n]"
+    where a = assertParse "assertProgramEmpty" (Q [])
+
+assertProgramSingle :: Assertion
+assertProgramSingle = do
+    a (Q [B True])        "[#t]"
+    -- a (Q [F 3.14])        "[3.14]"
+    a (Q [I 42])          "[42]"
+    a (Q [S "+"]) "[+]"
+    where a = assertParse "assertProgramSingle"
+
+assertProgramExtraWS :: Assertion
+assertProgramExtraWS = do
+    a (Q [B True])        "[ #t ]"
+    -- a (Q [F 3.14])        "[ 3.14 ]"
+    a (Q [I 42])          "[ 42 ]"
+    a (Q [S "+"]) "[ + ]"
+    where a = assertParse "assertProgramExtraWS"
+
+assertProgramEmbedded :: Assertion
+assertProgramEmbedded = do
+    a (Q [Q [B True]])         "[ [TRUE] ]"
+    a (Q [Q [I 42  ]])         "[ [ 42 ] ]"
+    -- a (Q [Q [Q [F 3.141592]]]) "[[[3.141592]] ]"
+    where a = assertParse "assertProgramEmbedded"
+
+assertProgramMultiple :: Assertion
+assertProgramMultiple = do
+    -- a (Q [I 42, I 13, F 3.141592]) "( 42 13 3.141592 )"
+    a (Q [I 42, I 13]) "[ 42 13 ]"
+    -- a (Q [I 42, Q [I 13, F 3.141592], S "+"]) "(42 (13 3.141592) +)"
+    a (Q [I 42, Q [I 13], S "+"]) "[42 [13 3.141592] +]"
+    a (Q [Q [S "a", S "b"], Q [S "d", S "e"], Q [S "f", S "g"]])
+      "[ [a b] [ d e ] [f g] ]"
+    where a = assertParse "assertProgramMultiple"
 
 --
 
@@ -156,18 +142,15 @@ parserTests =
                                 -- , testCase "float" assertLiteralFloat
                                 , testCase "integer" assertLiteralInt
                                 ]
-    , testGroup "instructions"  [
-                                -- testCase "alpha" assertInstructionAlpha
-                                -- , testCase "alphanum" assertInstructionAlphaNum
-                                -- , testCase "symbol" assertInstructionSymbol
-                                -- , testCase "dot" assertInstructionDot
+    , testGroup "instructions"  [ testCase "alpha" assertInstructionAlpha
+                                , testCase "alphanum" assertInstructionAlphaNum
+                                , testCase "symbol" assertInstructionSymbol
                                 ]
-    , testGroup "programs"      [
-                                -- testCase "empty" assertProgramEmpty
-                                -- , testCase "single" assertProgramSingle
-                                -- , testCase "extra-ws" assertProgramExtraWS
-                                -- , testCase "embedded" assertProgramEmbedded
-                                -- , testCase "multiple" assertProgramMultiple
+    , testGroup "programs"      [ testCase "empty" assertProgramEmpty
+                                , testCase "single" assertProgramSingle
+                                , testCase "extra-ws" assertProgramExtraWS
+                                , testCase "embedded" assertProgramEmbedded
+                                , testCase "multiple" assertProgramMultiple
                                 ]
     ]
 
